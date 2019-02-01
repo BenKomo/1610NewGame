9877b93136b7695237818cf036eb234ea3be7949
Benjamin <benjaminly6083@gmail.com>
2019-01-31 20:11:29 -0700

    Game Update January 31, 2019

--- 1610NewGame/Assets/SpaceShipEvent.cs
+++ 1610NewGame/Assets/SpaceShipEvent.cs
@@ -1,3 +1,12 @@
 ﻿using UnityEngine;
 using UnityEngine.Events;
 
+public class SpaceShipEvent : MonoBehaviour
+{
+    public UnityEvent Event;
+
+    private void OnCollisionEnter2D()
+    {
+        Event.Invoke();
+    }
+}
