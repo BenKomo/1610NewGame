cb9bcf3583ee599d943657d6820f5d96641f4e30
Benjamin <benjaminly6083@gmail.com>
2019-03-04 17:56:26 -0700

    3/4/2019 another update

---
+++ 1610NewGame/Assets/Scripts/Conditional.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conditional : MonoBehaviour
+{
+   public bool CanWalk;
+   public int Number = 11;
+   public string Password;
+
+   private void Update()
+   {
+      if (CanWalk)
+      {
+         print("True");
+      }
+      else
+      {
+         print("False");
+      }
+
+      if (Number >= 10)
+      {
+         print("Greater");
+      }
+      else
+      {
+         print("Lesser");
+      }
+      
+      if (Password ==  "OU812")
+      {
+         print("The password is correct");
+      }
+      else
+      {
+         print("The password is incorrect");
+      }
+   }
+}
