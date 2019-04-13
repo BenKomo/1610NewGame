d57ee48f69e75e11feceed22c77d1aa6d540a079
Benjamin <benjaminly6083@gmail.com>
2019-04-13 17:12:09 -0600

    added collection and list

---
+++ 1610NewGame/Assets/Scripts/FunctionsWithParam.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FunctionsWithParam : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       AddDate(12.5f, 8.5f); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void AddDate(float a, float b)
+    {
+        print(a+b);
+    }
+
+    private void OnTriggerExit(Collider obj)
+    {
+        obj.gameObject.SetActive(false);
+    }
+}
