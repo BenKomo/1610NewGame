8ffe4ff9ac579e63521706c39e5ac147211f5d3e
Benjamin <benjaminly6083@gmail.com>
2019-04-18 17:28:35 -0600

    Added Force into Project

---
+++ 1610NewGame/Assets/Scripts/FrocesNumberTwo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.TestTools.Utils;
+
+public class FrocesNumberTwo : MonoBehaviour
+{
+
+    public Vector3 ForceAmount;
+
+    private Rigidbody rBody;
+
+    private void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+    }
+
+    public void ApplyForce()
+    {
+        rBody.AddForce(ForceAmount);
+    }
+}
