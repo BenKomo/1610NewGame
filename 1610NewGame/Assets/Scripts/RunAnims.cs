08475f9c0e0670d45ea651aec6f519e4beb85139
Benjamin <benjaminly6083@gmail.com>
2019-04-23 14:43:53 -0600

    update, added Speed

---
+++ 1610NewGame/Assets/Scripts/RunAnims.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+
+public class RunAnims : MonoBehaviour
+{
+    private Animator anims;
+    public float Speed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        anims = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+       anims.SetFloat("Speed", Speed);
+    }
+
+   
+}
